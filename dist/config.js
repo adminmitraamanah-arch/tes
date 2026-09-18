529e6c0b921ba4f04285781601e72e73660aefe5
Hamzah Ilmi <admin.mitraamanah@gmail.com>
2026-09-19 00:49:04 +0700

    chore: hubungkan frontend ke Google Apps Script Web App URL produksi

--- dist/config.js
+++ dist/config.js
@@ -8,5 +8,5 @@
  */
 window.GAS_CONFIG = {
   // Ganti URL di bawah ini dengan URL Deployment Web App Google Apps Script Anda
-  API_URL: window.ENV_GAS_API_URL || "https://script.google.com/macros/s/AKfycbwPz26Bv00tNrm67w0j3n2Pq-dummy/exec"
+  API_URL: window.ENV_GAS_API_URL || "https://script.google.com/macros/s/AKfycbzx2fxfEkLnodb9dTiReOK9rtnWZp3aE1g7G2A_cntqG5lNdCBm792NPEuuCgtJNRxG/exec"
 };
